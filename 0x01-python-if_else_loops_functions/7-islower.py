15955d64d80bc743d7e9cc91f73e6c53520351b1
Collins Oduor Ochieng <125954874+Collins331@users.noreply.github.com>
2023-09-05 23:00:28 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for lowcase in c:
-        if 97 <= ord(lowcase) <= 122:
-            return True
-        else:
-            return False
+    if ord(c) >= 97 and ord(c) < 123:
+        return True
+    else:
+        return False
