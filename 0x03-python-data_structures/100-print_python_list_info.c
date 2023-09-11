1f9bb3b56214b20fd5f2fea5137ad8d307432051
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-11 22:35:20 +0300

    added 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,34 @@
+#include "Python.h"
+/**
+ * print_python_list_info - prints info
+ * Description:
+ * prints information about a python list
+ * The format is:
+ * [*] Size of the Python List is x
+ * [*] Allocated = x
+ * List of items with their type
+ *
+ * @p: pointer to list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *object;
+	struct _typeobject *type;
+
+	if (strcmp(p->ob_type->tp_name, "list") == 0)
+	{
+		list = (PyListObject *)p;
+		size = list->ob_base.ob_size;
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			object = list->ob_item[i];
+			type = object->ob_type;
+			printf("Element %ld: %s\n", i, type->tp_name);
+		}
+	}
+}
