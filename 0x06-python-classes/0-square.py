741b6ec6f0036f9e7fdb814e71e33c9c89b8f247
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-26 17:37:33 +0300

    added comment

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,4 +4,7 @@
 class Square:
     """ This code creates an empty Square class, which does nothing
     pass argument ensures the class does nothing"""
-    pass
+    def __init__(self):
+        """It takes one parameter for the self call
+        and pass command if given"""
+        pass
