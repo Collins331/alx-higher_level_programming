1263e23b9cc412cc7f67b624bc8e2e9d6451d045
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-26 17:44:16 +0300

    modified 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-    """ This code creates an empty Square class, which does nothing
-    pass argument ensures the class does nothing"""
+""" This code creates an empty Square class, which does nothing
+pass argument ensures the class does nothing"""
 
 
 class Square:
     """The class does nothing"""
-    
+
     pass
