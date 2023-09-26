b690428926706580331e89c40057c7cdf6cb26f6
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-26 23:37:34 +0300

    update files

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,9 +33,10 @@ class Square:
     """initializes position attribute to public"""
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        """modify the attribute value"""
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
