a9ae6704d6bd6429777946a9f897862744ce646e
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-10-09 21:38:45 +0300

    added 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""looks for available attributes and methods available in class"""
+
+def lookup(obj):
+    """return a list of attributes and methods of a class"""
+
+    return dir(obj)
