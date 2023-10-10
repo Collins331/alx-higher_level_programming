e4ddc6224c8a4bc8c83730f990fca90ee4495842
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-10-10 21:20:22 +0300

    modified 4-from_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
+"""Converts string object to JSON"""
 import json
 """import json module"""
 
 
 def to_json_string(my_obj):
-    """converts string object to JSON represention"""
+    """
+    converts string object to JSON represention
+    and return the JSON
+    """
     return (json.dumps(my_obj))
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
+"""JSON to string"""
 import json
 """imports json module"""
 
 
 def from_json_string(my_str):
-    """returns  an object represented by a JSON string"""
+    """
+    returns  an object represented
+    by a JSON string
+    """
     return (json.loads(my_str))
