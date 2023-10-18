9f0a6ccca1e28632261d0a2d47a12a5716e5b5f1
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-10-18 12:23:29 -0700

    update base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,9 @@
 import json
 """imports json module"""
 
+import csv
+"""imports the csv module"""
+
 
 class Base:
     """
@@ -69,3 +72,38 @@ class Base:
                 return [cls.create(**dic) for dic in lists]
         except FileNotFoundError:
             return "[]"
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + '.csv'
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            if cls.__name__ == "Rectangle":
+                for ob in list_objs:
+                    writer.writerow([ob.id, ob.width, ob.height, ob.x, ob.y])
+            elif cls.__name__ == "Square":
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = cls.__name__ + '.csv'
+        try:
+            with open(filename, 'r', newline='') as file:
+                reader = csv.reader(file)
+                objects = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(int(row[1]), int(row[2]))
+                        obj.id = int(row[0])
+                        obj.x = int(row[3])
+                        obj.y = int(row[4])
+                    elif cls.__name__ == "Square":
+                        obj = cls(int(row[1]))
+                        obj.id = int(row[0])
+                        obj.x = int(row[2])
+                        obj.y = int(row[3])
+                    objects.append(obj)
+                return objects
+        except FileNotFoundError:
+            return []
