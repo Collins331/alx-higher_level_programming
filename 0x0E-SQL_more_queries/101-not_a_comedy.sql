f2df6b91ea9e28d3cddaba0887e6820f820c58a7
Collins <ochiengcollinsoduor2@gmail.com>
2023-12-11 15:23:04 +0300

    no comedy genre

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+SELECT ts.title
+FROM tv_shows AS ts
+WHERE ts.title NOT IN 
+(
+    SELECT ts.title
+    FROM tv_shows AS ts
+    INNER JOIN tv_show_genres AS tsg
+    ON ts.id = tsg.show_id
+    INNER JOIN tv_genres AS tg
+    ON tg.id = tsg.genre_id
+    WHERE tg.name = "Comedy"
+)
+GROUP BY ts.title
+ORDER BY ts.title ASC;
