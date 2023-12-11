9a2d30b234b83d508fce717d9e97585809bce9a4
Collins <ochiengcollinsoduor2@gmail.com>
2023-12-11 07:09:45 +0300

    left join

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+--  a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON ts.id = tsg.show_id
+ORDER BY ts.title , tsg.genre_id ASC;
