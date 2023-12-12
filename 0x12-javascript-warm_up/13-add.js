e5842b5fb63630ee47b8ed43ddbff4fdcbc55961
Collins <ochiengcollinsoduor2@gmail.com>
2023-12-12 23:39:10 +0300

    update

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-function add (a, b) {
-  return a + b;
-}
-
-exports.add = add;
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const process = require('process');
-
-const firstArg = process.argv;
-const secondArg = process.argv;
-console.log(firstArg[2] + ' is ' + secondArg[3]);
