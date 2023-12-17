92335b120af43bbfc9221cf3468c580ffb9fe4fc
Collins <ochiengcollinsoduor2@gmail.com>
2023-12-18 00:52:59 +0300

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# instance of Base = declarative_base()
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
