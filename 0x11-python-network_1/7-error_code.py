b80df6ac4a8bbe87abc438bfb8019b137db9536f
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-20 15:11:38 +0300

    Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""import sys and request modules"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    script that takes in a URL, sends a request to the URL and displays the body
+    """
+    try:
+        r = requests.get(argv[1])
+        if r.text != None:
+            print(r.text)
+        else:
+            r.raise_for_status()
+    except HTTPError:
+        print("Error code: {}".format(r.status_code)
