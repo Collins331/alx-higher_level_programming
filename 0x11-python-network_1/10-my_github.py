b13a89f234961b4234c9b37456ef30558b6d6895
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-20 19:55:06 +0300

    interview

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""import sys and requests modules"""
+from sys import argv as ag
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    # get the username and password from the command line
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    # GitHub API URL
+    url = 'https://api.github.com/user'
+
+    # authentication
+    auth = HTTPBasicAuth(username, password)
+
+    # send a GET request to the GitHub API
+    response = requests.get(url, auth=auth)
+    print(response.json().get("id"))
