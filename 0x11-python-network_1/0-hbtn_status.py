18e1ca0f751a4540184245fbb255a79ea9ca7d78
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-20 12:20:04 +0300

    update fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,5 @@ with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     body = response.read()
 
 print(
-    "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
-        type(body), body, body.decode("utf-8")
-    )
-)
+    "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    .format(type(body), body, body.decode("utf-8")))
