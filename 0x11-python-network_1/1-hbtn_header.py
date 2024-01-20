12eb12eec4bd4708c96072fa30c492d83984cf3b
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-20 12:33:43 +0300

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
 import urllib.request
-"""import the sys module and urllib module"""
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    header = response.info()
+if __name__ == "__main__":
+    """import the sys module and urllib module"""
 
-print(header['X-Request-Id'])
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+
+    print(header['X-Request-Id'])
