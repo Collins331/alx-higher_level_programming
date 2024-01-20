08bd407025c1f264b780d151b3fa87170520b167
Collins <ochiengcollinsoduor2@gmail.com>
2024-01-20 15:21:24 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,11 +7,8 @@ if __name__ == "__main__":
     """
     script that takes in a URL, sends request to the URL and displays the body
     """
-    try:
-        r = requests.get(argv[1])
-        if r.text is not None:
-            print(r.text)
-        else:
-            r.raise_for_status()
-    except HTTPError as e:
-        print("Error code: {}".format(e.status_code))
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
